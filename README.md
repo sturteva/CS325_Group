e8da4d9ab61a180ed1255eb707bf3454b746e7da
Lauren Mackey <lauren.m.mackey@gmail.com>
2018-06-06 19:29:14 -0700

    Cleaned up code and updated README

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 As of right now we only support python 2
 
 run
-`python tsp.py /path/to/inputs.txt`
+`python tsp.py path/to/inputs.txt`
 to run the TSP module
 ex:
-`python tsp.py mytest.txt`
+`python tsp.py tests/mytest.txt`
 from the root directory
+
+output will generate in tests/mytest.txt.tour
 
 ## Tests
 To run the tests,
@@ -16,4 +18,8 @@ run
 
 ## TSP-Verifier
 To verify that all locations were added to the solution file
-`python tsp-verifier [input filename] [solution filename]`
+`python tsp-verifier.py [input filepath] [solution filepath]`
+
+## Timing
+To verify that all algorithms run in the required 3 minute time
+`python watch.py python tsp.py [input filepath]`
--- tsp.py
+++ tsp.py
@@ -16,6 +16,7 @@ class Vertex(object):
         self.y_coord = y_coord
         self.degree = 0
 
+
 # An Edge Object
 class Edge(object):
 
@@ -23,25 +24,20 @@ class Edge(object):
     def __init__(self, v1, v2):
         self.v1 = v1
         self.v2 = v2
-        self.distance = round(math.sqrt(math.pow((v1.x_coord - v2.x_coord), 2) + math.pow((v1.y_coord - v2.y_coord), 2)))
+        self.distance = round(math.sqrt(math.pow((v1.x_coord - v2.x_coord), 2) + math.pow((v1.y_coord - v2.y_coord), 2)), 0)
 
 
 def create_edge_list(vertex_list):
-    """ Creates a list of non redundant egdes connecting every vertex to other all vertices
+    """ Creates a list of non redundant edges connecting every vertex to all other vertices
     Our TSP graph is undirected, so one edge is enough to represent the connection
     between two vertices.
     """
-
    
     edge_list = []
     num = len(vertex_list)
   
     connection_matrix = [[False for y in range(num)] for x in range(num)]
  
-    
-    # TODO: Should only need to go through the first half of the vertices to
-    # create a complete Graph
-    # For instance, you won't need edge (A, B) & (B, A)
     for start in vertex_list:
         for destination in vertex_list:
 
@@ -55,7 +51,6 @@ def create_edge_list(vertex_list):
                 edge_list.append(new_edge)
                 connection_matrix[start.id][destination.id] = True
                 connection_matrix[destination.id][start.id] = True 
-
 
     return edge_list
 
@@ -89,7 +84,7 @@ def does_create_cycle(tour_list, new_edge):
             return False
         else:
             if current_e.v1 == new_edge.v2 or current_e.v2 == new_edge.v2:
-	             return True
+                 return True
 
 
 def create_tour(edge_list, num_cities):
@@ -101,8 +96,8 @@ def create_tour(edge_list, num_cities):
 
             has_cycle = does_create_cycle(tour_list, edge)
 
-            should_go_home = len(tour_list)+ 1 == num_cities
-	  	    
+            should_go_home = len(tour_list) + 1 == num_cities
+            
             if not has_cycle or (has_cycle and should_go_home):
                 edge.v1.degree += 1
                 edge.v2.degree += 1
@@ -111,184 +106,194 @@ def create_tour(edge_list, num_cities):
 
     return tour_list, distance
 
+
 def get_distance(v1,v2):
+    return round(math.sqrt(math.pow((v1.x_coord - v2.x_coord), 2) + math.pow((v1.y_coord - v2.y_coord), 2)))
 
-	return round(math.sqrt(math.pow((v1.x_coord - v2.x_coord), 2) + math.pow((v1.y_coord - v2.y_coord), 2)))
 
-def two_opt(tour_list, edge_list,startTime):
+def two_opt(tour_list, edge_list,start_time):
+    #going to make a copy of our tour_list
+    curbest = []
+    for each in tour_list:
+        curbest.append(each)
+        current_time = timeit.default_timer()
 
-	#going to make a copy of our tour_list
-	curbest = []
-	for each in tour_list:
-		curbest.append(each)
+    i = 0
+
+    while (current_time - start_time) * 1000 < 140000 and i < len(curbest):
+        print i
+        #Here we need to decide which two edges to 'swap' and check
+        #right now it is random
+        edge1 = curbest[len(curbest)-1-i]
+        edge2 = curbest[len(curbest)-i-2]
+        v1 = edge1.v1
+        v2 = edge1.v2
+        v3 = edge2.v1
+        v4 = edge2.v2
+
+        target_distance = edge1.distance + edge2.distance
+        
+        if v1 == v3 or v1 == v4 or v2 == v3 or v2 == v4:
+            i += 1
+            continue
+
+        else:
+            print 'got here'
+            #odds/Even distance
+            odd_distance = get_distance(v1,v3)
+            even_distance = get_distance(v2,v4)
+            odd_even = odd_distance + even_distance
+
+            #inside/outside distance
+            inside_distance = get_distance(v2,v3)
+            outside_distance = get_distance(v1,v4)
+            inside_outside = inside_distance + outside_distance
+
+            #checks to see which is smaller
+            if odd_even < inside_outside:
+                print 'oddEVen smaller'
+                #now checks if less than currentdistance
+                if odd_even < target_distance:
+                    new_edge1 = Edge(v1,v3)
+                    curbest.remove(edge1)
+                    cycle1 = does_create_cycle(curbest, new_edge1)
+
+                    #A cycle is not created, reset and restart
+                    if not cycle1:
+                        curbest.append(edge1)
+                        continue
+                    else:
+                        curbest.append(new_edge1)
+
+                    new_edge2 = Edge(v2,v4)
+                    curbest.remove(edge2)
+                    cycle2 = does_create_cycle(curbest, new_edge2)
+
+                    #A cycle is not created, reset and restart
+                    if not cycle2:
+                        curbest.remove(new_edge1)
+                        curbest.append(edge1)
+                        curbest.append(edge2)
+                        continue
+                    else:
+                        curbest.append(new_edge1)
+            
+            elif inside_outside < target_distance:
+                print("insideOutside smaller")
+                
+                new_edge1 = Edge(v2,v3)
+                curbest.remove(edge1)
+                cycle1 = does_create_cycle(curbest, new_edge1)
+        
+                #A cycle is not created, reset and restart
+                if not cycle1:
+                    curbest.append(edge1)
+                    continue
+                else:
+                    curbest.append(new_edge1)
+
+                new_edge2 = Edge(v1,v4)
+                curbest.remove(edge2)
+                cycle2 = does_create_cycle(curbest,new_edge2)
+
+                #A cycle is not created, reset and restart
+                if not cycle2:
+                    curbest.remove(new_edge1)
+                    curbest.append(edge1)
+                    curbest.append(edge2)
+                    continue
+                else:
+                    curbest.append(new_edge1)               
+                            
+        i += 1
         currentTime = timeit.default_timer()
-
-	i = 0
-
-	while (currentTime-startTime)*1000 < 140000 and i < len(curbest):
-		print (i)
-		#Here we need to decide which two edges to 'swap' and check
-		#right now it is random
-		edge1 = curbest[len(curbest)-1-i]
-		edge2 = curbest[len(curbest)-i-2]
-		v1 = edge1.v1
-		v2 = edge1.v2
-		v3 = edge2.v1
-		v4 = edge2.v2
-
-		targetDistance = edge1.distance + edge2.distance
-		
-		if v1 == v3 or v1 == v4 or v2 == v3 or v2 == v4:
-			i += 1
-			continue
-
-		else:
-			print ("got here")
-			#odds/Even distance
-			oddDistance = get_distance(v1,v3)
-			evenDistance = get_distance(v2,v4)
-			oddEven = oddDistance + evenDistance
-
-			#inside/outside distance
-			insideDistance = get_distance(v2,v3)
-			outsideDistance = get_distance(v1,v4)
-			insideOutside = insideDistance + outsideDistance
-
-			#checks to see which is smaller
-			if oddEven < insideOutside:
-				print("oddEVen smaller")
-				#now checks if less than currentdistance
-				if oddEven < targetDistance:
-					new_edge1 = Edge(v1,v3)
-
-	             	                curbest.remove(edge1)
-        	        	        cycle1 = does_create_cycle(curbest,new_edge1)
-
-                	                #A cycle is not created, reset and restart
-                        	        if not cycle1:
-                                	        curbest.append(edge1)
-	                                       	continue
-        	                        else:
-                	                        curbest.append(new_edge1)
-
-                        	        new_edge2 = Edge(v2,v4)
-	                                curbest.remove(edge2)
-	                                cycle2 = does_create_cycle(curbest,new_edge2)
-
-        	                        #A cycle is not created, reset and restart
-                	                if not cycle2:
-                        	                curbest.remove(new_edge1)
-                                	        curbest.append(edge1)
-                                        	curbest.append(edge2)
-	                                        continue
-        	                        else:
-                	                        curbest.append(new_edge1)
+         
+    return curbest
 
 
-			
-			elif insideOutside < targetDistance:
-				print("insideOutside smaller")
-				
-				new_edge1 = Edge(v2,v3)
-
-				curbest.remove(edge1)
-				cycle1 = does_create_cycle(curbest,new_edge1)
-		
-				#A cycle is not created, reset and restart
-				if not cycle1:
-					curbest.append(edge1)
-					continue
-				else:
-					curbest.append(new_edge1)
-
-				new_edge2 = Edge(v1,v4)
-				curbest.remove(edge2)
-				cycle2 = does_create_cycle(curbest,new_edge2)
-
-				#A cycle is not created, reset and restart
-				if not cycle2:
-					curbest.remove(new_edge1)
-					curbest.append(edge1)
-					curbest.append(edge2)
-					continue
-				else:
-					curbest.append(new_edge1)
-					
-				
-							
-		i += 1
-		currentTime = timeit.default_timer()
-		 
-	return curbest
-def print_tour(o, tour_list, search_city):
+def write_tour(o, tour_list, search_city):
     """Adds tour in order to output file, skipping first city when listed at end"""
-
-#    o.write('%i\n' % search_city.id)
     while (len(tour_list) > 1):
-	
         for remaining_edge in tour_list:
             if remaining_edge.v1.id == search_city.id:
-		o.write('%i\n' % remaining_edge.v1.id)
+                o.write('%i\n' % remaining_edge.v1.id)
                 tour_list.remove(remaining_edge)
-		search_city = remaining_edge.v2
-	        break
+                search_city = remaining_edge.v2
             elif remaining_edge.v2.id == search_city.id:
                 o.write('%i\n' % remaining_edge.v2.id)
                 tour_list.remove(remaining_edge)
                 search_city = remaining_edge.v1
-                break
     o.write('%i\n' % search_city.id)
-   	 
+    o.close()
+
+
+def print_checks(run_time, distance):
+    print 'Runtime in milliseconds is: ', run_time * 1000
+    print 'Runtime in seconds is: ', run_time
+
+    if sys.argv[1] == 'tsp_example_1.txt':
+        print 'Optimal distance is: ', 108159
+        print 'Our distance is: ', distance
+        print 'Ratio is: ', distance / 108159
+
+    elif sys.argv[1] == 'tsp_example_2.txt':
+        print 'Optimal distance is: ', 2579
+        print 'Our distance is: ', distance
+        print 'Ratio is: ', distance / 2579
+
+    elif sys.argv[1] == 'tsp_example_3.txt':
+        print 'Optimal distance is: ', 1573084
+        print 'Our distance is: ', distance
+        print 'Ratio is: ', distance / 1573084
+
+    else:
+        print 'Our distance is: ', distance
+     
 
 # Driver Code
 if __name__ == '__main__':
-    startTime = timeit.default_timer()	
+    start_time = timeit.default_timer()  
     cities = []
-    f = open(sys.argv[1], 'r')
 
-    # Get our Input
-    for line in f:
-        item = line.split()
-
-        for i in range(len(item)):
-					
-            item[i] = int(item[i])
-
-        id = item[0]
-	x_coord = item[1]
-        y_coord = item[2]
-        new_city = Vertex(id, x_coord, y_coord)
-        cities.append(new_city)
-
-    f.close()
+    with open(sys.argv[1], 'r') as f:
+        # Get our Input
+        for line in f:
+            item = line.split()
+    
+            for i in range(len(item)):                    
+                item[i] = int(item[i])
+    
+            id = item[0]
+            x_coord = item[1]
+            y_coord = item[2]
+            new_city = Vertex(id, x_coord, y_coord)
+            cities.append(new_city)
+    
+        f.close()
 
     edge_list = create_edge_list(cities)
     mergesort(edge_list, comparator=distance_comparator)
     tour_list, distance = create_tour(edge_list, len(cities))
 
     #Checks if we are under 3 minutes to continue optimization
-    #Leaving 40 seconds to write to the file(this number can be changed
+    #Leaving 40 seconds to write to the file (this number can be changed)
     timeCheck = timeit.default_timer()
-    if (timeCheck-startTime)*1000 < 140000:
-        tour_list = two_opt(tour_list,edge_list,startTime)
-	
-	#need to calculate new distance
-	distance = 0
-	for each in tour_list:
-		distance += each.distance
+    if (timeCheck - start_time) * 1000 < 140000:
+        tour_list = two_opt(tour_list, edge_list, start_time)
+    
+    #need to calculate new distance
+    distance = 0
+    for each in tour_list:
+        distance += each.distance
 
-	print(distance)
-
-
-    # Print output
+    # Write to output file
     with open(sys.argv[1] + '.tour', 'w') as o:
         o.write('%i\n' % distance)
         current_edge = tour_list[0]
         o.write('%i\n' % current_edge.v1.id)
         tour_list.remove(current_edge)
-        print_tour(o, tour_list, current_edge.v2)
+        write_tour(o, tour_list, current_edge.v2)
 
-    endTime = timeit.default_timer()
-    runTime = (endTime - startTime) * 1000
-    print "TheRuntime in Milliseconds is:"
-    print runTime
+    end_time = timeit.default_timer()
+    run_time = (end_time - start_time)
+    print_checks(run_time, distance)
